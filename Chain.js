9616b706f7a547279ba0c1473bc41b5c394192e8
Subhodip Gorai <bhairabnathgorai75@gmail.com>
2021-06-29 15:52:20 +0530

    abc

--- Chain.js
+++ Chain.js
@@ -1,20 +1,25 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
+        this.pointB = pointB;
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
-
+    fly(){
+        this.chain.bodyA = null
+    }
     display(){
-        var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        if (this.chain.bodyA) {
+            var A = this.chain.bodyA.position;
+        var B = this.pointB;
         strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        line(A.x, A.y, B.x, B.y);
+        }
     }
     
 }
